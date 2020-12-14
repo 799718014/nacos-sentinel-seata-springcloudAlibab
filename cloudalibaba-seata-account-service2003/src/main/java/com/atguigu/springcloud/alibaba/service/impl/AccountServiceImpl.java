651201c1cb66d728b2d99f44825e87d8da3ad784
liuzx <liuzx@usoftchina.com>
2020-12-14 16:14:59 +0800

    1、Account项目的mybatis接入mybatis-generator

--- cloudalibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ cloudalibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -1,10 +1,10 @@
 package com.atguigu.springcloud.alibaba.service.impl;
 
-import com.atguigu.springcloud.alibaba.dao.AccountDao;
+import com.atguigu.springcloud.alibaba.dao.mapper.AccountMapper;
 import com.atguigu.springcloud.alibaba.service.AccountService;
-import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,8 +15,9 @@ import java.util.concurrent.TimeUnit;
 public class AccountServiceImpl implements AccountService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceImpl.class);
 
-    @Resource
-    AccountDao accountDao;
+    @Autowired
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    private AccountMapper accountMapper;
 
     /**
      * 扣减余额
@@ -27,7 +28,7 @@ public class AccountServiceImpl implements AccountService {
         //模拟超时异常，全局事务回滚
         //暂停几秒钟线程
         //try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
-        accountDao.decrease(userId,money);
+        accountMapper.decrease(userId,money);
         LOGGER.info("------->account-service中扣减账户余额结束");
     }
 
